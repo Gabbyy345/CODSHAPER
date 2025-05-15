ea98ec34763ae5fcb8ec2bb5bc08638548a946c4
NEON2DW3RLD <109807354+NEON2DW3RLD@users.noreply.github.com>
2025-05-15 15:29:44 +0100

    Vite update

---
+++ postcss.config.cjs
@@ -0,0 +1,27 @@
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+// import tailwind from '@tailwindcss/postcss'
+// import autoprefixer from 'autoprefixer'
+
+// export default {
+//   plugins: [
+//     tailwind,
+//     autoprefixer
+//   ]
+// }
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
